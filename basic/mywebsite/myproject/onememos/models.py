fd9d108f70dd90dcbf77db56cadba09b30e9070f
Buyoung Chung <sunarol@naver.com>
2024-01-02 21:39:41 +0900

    feat: Create table
    모델 정의, admin에 모델 등록, settings에 App 등록

--- basic/mywebsite/myproject/onememos/models.py
+++ basic/mywebsite/myproject/onememos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Memo(models.Model):
+    memo_text = models.CharField(max_length=200)
+    # 인스턴스 생성시 날짜가 자동으로 되게끔 auto_now_add=True 설정
+    published_date = models.DateTimeField(auto_now_add=True)
